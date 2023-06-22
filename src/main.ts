45f14cdf141633edcd731a131fd528868df6cffa
unknown <batu73580@gmail.com>
2023-06-22 09:45:52 +0400

    product website

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ let status = "less";
 
 const searchProduct = document.querySelector("input") as HTMLInputElement;
 const cardContainer = document.querySelector(".container") as HTMLDivElement;
+const btnSearch = document.querySelector("button") as HTMLButtonElement;
 
 const productCommerce = async () => {
   try {
@@ -12,57 +13,76 @@ const productCommerce = async () => {
     const response = await axios.get(
       `https://fakestoreapi.com/products?q=${input}`
     );
-    response.data.forEach((product: Root2) => {
-      const card = document.createElement("div");
-      card.className = "card";
 
-      const imageWrapper = document.createElement("div");
-      imageWrapper.className = "image-wrapper";
-      card.appendChild(imageWrapper);
+    cardContainer.innerHTML = "";
 
-      const image = document.createElement("img");
-      image.src = product.image;
-      image.alt = "Product 1";
-      imageWrapper.appendChild(image);
+    if (!response.data || response.data.length === 0) {
+      const notFoundText = document.createElement("p");
+      notFoundText.textContent = "Product not found";
+      cardContainer.appendChild(notFoundText);
+    } else {
+      response.data.forEach((product: Root2) => {
+        const card = document.createElement("div");
+        card.className = "card";
 
-      const title = document.createElement("h2");
-      title.textContent = product.title;
-      card.appendChild(title);
+        const imageWrapper = document.createElement("div");
+        imageWrapper.className = "image-wrapper";
+        card.appendChild(imageWrapper);
 
-      const description = document.createElement("p");
-      description.textContent = product.description;
-      card.appendChild(description);
+        const image = document.createElement("img");
+        image.src = product.image;
+        image.alt = "Product 1";
+        imageWrapper.appendChild(image);
 
-      const seeMore = document.createElement("button");
-      seeMore.innerHTML = "See More";
-      card.appendChild(seeMore);
+        const title = document.createElement("h2");
+        title.textContent = product.title;
+        card.appendChild(title);
 
-      seeMore.addEventListener("click", () => {
-        toggleText();
+        const description = document.createElement("p");
+        description.textContent = product.description;
+        card.appendChild(description);
+
+        const seeMore = document.createElement("button");
+        seeMore.innerHTML = "See More";
+        card.appendChild(seeMore);
+
+        seeMore.addEventListener("click", () => {
+          toggleText();
+        });
+
+        const toggleText = () => {
+          if (status === "less") {
+            description.style.whiteSpace = "normal";
+            seeMore.innerHTML = "See Less";
+            status = "more";
+          } else if (status === "more") {
+            description.style.whiteSpace = "nowrap";
+            seeMore.innerHTML = "See More";
+            status = "less";
+          }
+        };
+
+        const price = document.createElement("p");
+        price.className = "price";
+        price.innerHTML = `&#36;${product.price}`;
+        card.appendChild(price);
+
+        cardContainer.appendChild(card);
       });
-
-      const toggleText = () => {
-        if (status === "less") {
-          description.style.whiteSpace = "normal";
-          seeMore.innerHTML = "See Less";
-          status = "more";
-        } else if (status === "more") {
-          description.style.whiteSpace = "nowrap";
-          seeMore.innerHTML = "See More";
-          status = "less";
-        }
-      };
-
-      const price = document.createElement("p");
-      price.className = "price";
-      price.innerHTML = `&#36;${product.price}`;
-      card.appendChild(price);
-
-      cardContainer.appendChild(card);
-    });
+    }
   } catch (error) {
     console.error(error);
   }
 };
+
+searchProduct.addEventListener("submit", (e) => {
+    e.preventDefault();
+    productCommerce();
+  }
+)
+
+btnSearch.addEventListener("click", () => {
+  productCommerce();
+});
 
 productCommerce();
