ec1365d09c2b1a45ae1c93e1438018472378dea6
unknown <batu73580@gmail.com>
2023-06-23 21:59:47 +0400

    Product Website

--- src/main.ts
+++ src/main.ts
@@ -2,87 +2,103 @@ import axios from "axios";
 import { Root2 } from "./response.type";
 
 let status = "less";
+let allProducts: Root2[] = [];
+let filteredProducts: Root2[] = [];
 
 const searchProduct = document.querySelector("input") as HTMLInputElement;
 const cardContainer = document.querySelector(".container") as HTMLDivElement;
 const btnSearch = document.querySelector("button") as HTMLButtonElement;
 
-const productCommerce = async () => {
+const fetchData = async () => {
   try {
-    const input = searchProduct.value;
     const response = await axios.get(
-      `https://fakestoreapi.com/products?q=${input}`
+      "https://api.escuelajs.co/api/v1/products"
     );
-
-    cardContainer.innerHTML = "";
-
-    if (!response.data || response.data.length === 0) {
-      const notFoundText = document.createElement("p");
-      notFoundText.textContent = "Product not found";
-      cardContainer.appendChild(notFoundText);
-    } else {
-      response.data.forEach((product: Root2) => {
-        const card = document.createElement("div");
-        card.className = "card";
-
-        const imageWrapper = document.createElement("div");
-        imageWrapper.className = "image-wrapper";
-        card.appendChild(imageWrapper);
-
-        const image = document.createElement("img");
-        image.src = product.image;
-        image.alt = "Product 1";
-        imageWrapper.appendChild(image);
-
-        const title = document.createElement("h2");
-        title.textContent = product.title;
-        card.appendChild(title);
-
-        const description = document.createElement("p");
-        description.textContent = product.description;
-        card.appendChild(description);
-
-        const seeMore = document.createElement("button");
-        seeMore.innerHTML = "See More";
-        card.appendChild(seeMore);
-
-        seeMore.addEventListener("click", () => {
-          toggleText();
-        });
-
-        const toggleText = () => {
-          if (status === "less") {
-            description.style.whiteSpace = "normal";
-            seeMore.innerHTML = "See Less";
-            status = "more";
-          } else if (status === "more") {
-            description.style.whiteSpace = "nowrap";
-            seeMore.innerHTML = "See More";
-            status = "less";
-          }
-        };
-
-        const price = document.createElement("p");
-        price.className = "price";
-        price.innerHTML = `&#36;${product.price}`;
-        card.appendChild(price);
-
-        cardContainer.appendChild(card);
-      });
-    }
+    allProducts = response.data;
   } catch (error) {
     console.error(error);
   }
 };
 
-searchProduct.addEventListener("submit", (e) => {
-    e.preventDefault();
-    productCommerce();
-  }
-)
+const renderProducts = (products: Root2[]) => {
+  cardContainer.innerHTML = "";
 
-btnSearch.addEventListener("click", () => {
-  productCommerce();
+  if (products.length === 0) {
+    const message = document.createElement("p");
+    message.textContent = "No products found.";
+    cardContainer.appendChild(message);
+    return;
+  }
+
+  products.forEach((product: Root2) => {
+    const card = document.createElement("div");
+    card.className = "card";
+
+    const imageWrapper = document.createElement("div");
+    imageWrapper.className = "image-wrapper";
+    card.appendChild(imageWrapper);
+
+    const image = document.createElement("img");
+    image.src = product.images;
+    image.alt = "Product 1";
+    imageWrapper.appendChild(image);
+
+    const title = document.createElement("h2");
+    title.textContent = product.title;
+    card.appendChild(title);
+
+    const description = document.createElement("p");
+    description.textContent = product.description;
+    card.appendChild(description);
+
+    const seeMore = document.createElement("button");
+    seeMore.innerHTML = "See More";
+    card.appendChild(seeMore);
+
+    seeMore.addEventListener("click", () => {
+      toggleText(description, seeMore);
+    });
+
+    const toggleText = (desc: HTMLParagraphElement, btn: HTMLButtonElement) => {
+      if (status === "less") {
+        desc.style.whiteSpace = "normal";
+        btn.innerHTML = "See Less";
+        status = "more";
+      } else if (status === "more") {
+        desc.style.whiteSpace = "nowrap";
+        btn.innerHTML = "See More";
+        status = "less";
+      }
+    };
+
+    const price = document.createElement("p");
+    price.className = "price";
+    price.innerHTML = `&#36;${product.price}`;
+    card.appendChild(price);
+
+    cardContainer.appendChild(card);
+  });
+};
+
+const filterProducts = (input: string) => {
+  filteredProducts = allProducts.filter((product: Root2) =>
+    product.title.toLowerCase().includes(input.toLowerCase())
+  );
+  renderProducts(filteredProducts);
+};
+
+const handleSearch = () => {
+  const input = searchProduct.value.trim();
+  filterProducts(input);
+};
+
+searchProduct.addEventListener("keydown", (e) => {
+  if (e.key === "Enter") {
+    e.preventDefault();
+    handleSearch();
+  }
 });
 
-productCommerce();
+btnSearch.addEventListener("click", handleSearch);
+
+fetchData();
--- src/response.type.ts
+++ src/response.type.ts
@@ -1,15 +1,15 @@
 export type Root = Root2[]
 
-
 export interface Root2 {
   id: number
   title: string
   price: number
   description: string
   category: string
-  image: string
+  images: string
   rating: Rating
 }
+
 
 export interface Rating {
   rate: number
